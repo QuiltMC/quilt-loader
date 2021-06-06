189e14f2eb1038c8c5f2c96447dde6f49a511e6f
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2021-06-06 12:34:35 -0700

    Remove unused import

--- src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
+++ src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
