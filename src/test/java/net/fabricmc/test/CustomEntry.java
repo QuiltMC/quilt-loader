9935150a819c0199dd3b7b657e312ae9f95c554b
TheGlitch76 <glitch.g3431@gmail.com>
2021-06-17 22:52:13 -0500

    Fix tests and licensing

--- src/test/java/net/fabricmc/test/CustomEntry.java
+++
@@ -1,9 +0,0 @@
-package net.fabricmc.test;
-
-@FunctionalInterface
-public interface CustomEntry {
-	String name();
-	default String describe() {
-		return "Custom entry point \"" + name() + '"';
-	}
-}
