3b044570d7808909d7b821cb8c9ae08ed994eba4
ineGame159 <petulko08@gmail.com>
2021-06-17 22:49:19 -0500

    Access wideners can now target classes in 'com.mojang.blaze3d' package

--- src/main/java/org/quiltmc/loader/impl/transformer/QuiltTransformer.java
+++ src/main/java/org/quiltmc/loader/impl/transformer/QuiltTransformer.java
@@ -44,7 +44,7 @@ public final class QuiltTransformer {
 	}
 
 	public static byte[] transform(boolean isDevelopment, EnvType envType, String name, byte[] bytes) {
-		boolean isMinecraftClass = name.startsWith("net.minecraft.") || name.indexOf('.') < 0;
+		boolean isMinecraftClass = name.startsWith("net.minecraft.") || name.startsWith("com.mojang.blaze3d.") || name.indexOf('.') < 0;
 		boolean transformAccess = isMinecraftClass && QuiltLauncherBase.getLauncher().getMappingConfiguration().requiresPackageAccessHack();
 		boolean environmentStrip = !isMinecraftClass || isDevelopment;
 		boolean applyAccessWidener = isMinecraftClass && QuiltLoaderImpl.INSTANCE.getAccessWidener().getTargets().contains(name);
