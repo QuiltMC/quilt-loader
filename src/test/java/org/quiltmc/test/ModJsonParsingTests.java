16489226736848b850881c5d6dbdf290083c47b8
Glitch <glitchieproductionsofficial@gmail.com>
2021-07-27 17:16:35 -0500

    Automatic tests for quilt.mod.json parsing

--- src/test/java/org/quiltmc/test/ModJsonParsingTests.java
+++ src/test/java/org/quiltmc/test/ModJsonParsingTests.java
@@ -2,14 +2,15 @@ package org.quiltmc.test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.quiltmc.loader.impl.metadata.FabricModMetadataReader;
 import org.quiltmc.loader.impl.metadata.qmj.ModMetadataReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class ModJsonParsingTests {
 	private static final Logger LOGGER = LogManager.getLogger();
@@ -33,10 +34,20 @@ public class ModJsonParsingTests {
 		errorPath = testLocation.resolve("error");
 	}
 
-	@Test
-	void optionalFieldsArentRequired() throws IOException {
-		// In an early prototype some optional fields were accidentally required in dependency objects, this
-		// tests that they are actually optional
-		ModMetadataReader.read(LOGGER, testLocation.resolve("optional_fields.json"));
+	@TestFactory
+	Stream<DynamicTest> autoTests() throws IOException {
+		Path loc = testLocation.resolve("auto");
+		Stream<DynamicTest> spec = DynamicTest.stream(Files.walk(loc.resolve("spec")).filter(path -> !Files.isDirectory(path) && path.toString().endsWith(".json")),
+				p -> p.getFileName().toString(), p -> ModMetadataReader.read(LOGGER, p));
+		Stream<DynamicTest> error =	DynamicTest.stream(Files.walk(loc.resolve("spec")).filter(path -> !Files.isDirectory(path) && path.toString().endsWith(".json")),
+				p -> p.getFileName().toString(), p -> {
+					try {
+						ModMetadataReader.read(LOGGER, p);
+						Assertions.fail("Erroneous quilt.mod.json was parsed successfully");
+					} catch (Exception ex) {
+						// do nothing
+					}
+				});
+		return Stream.concat(spec, error);
 	}
 }
