db59e71d839a8868d212525d6fe9649ec191a0df
Glitch <glitchieproductionsofficial@gmail.com>
2022-02-16 19:25:54 -0600

    Add fabric KnotClient/KnotServer for Loom compatability

---
+++ src/main/java/net/fabricmc/loader/launch/knot/KnotClient.java
@@ -0,0 +1,11 @@
+package net.fabricmc.loader.launch.knot;
+
+import net.fabricmc.api.EnvType;
+
+import org.quiltmc.loader.impl.launch.knot.Knot;
+
+public class KnotClient {
+	public static void main(String[] args) {
+		Knot.launch(args, EnvType.CLIENT);
+	}
+}
