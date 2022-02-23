b6aa624e4e16f8e01e738e2a71f989ffbe8b016c
AlexIIL <alexj9@me.com>
2022-02-23 00:04:42 +0000

    Remove old imports.

--- src/main/java/org/quiltmc/loader/impl/gui/QuiltStatusTree.java
+++ src/main/java/org/quiltmc/loader/impl/gui/QuiltStatusTree.java
@@ -17,8 +17,6 @@
 package org.quiltmc.loader.impl.gui;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -27,7 +25,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
-import java.util.StringJoiner;
 import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
