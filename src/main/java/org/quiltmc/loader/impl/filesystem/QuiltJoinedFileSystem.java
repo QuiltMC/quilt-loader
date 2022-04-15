ab4bf42900481289970369259217279b3c54b230
Glitch <glitch.g3431@gmail.com>
2022-04-14 21:17:21 -0500

    Fix invalid URI syntax in QuiltJoinedFileSystem

--- src/main/java/org/quiltmc/loader/impl/filesystem/QuiltJoinedFileSystem.java
+++ src/main/java/org/quiltmc/loader/impl/filesystem/QuiltJoinedFileSystem.java
@@ -60,7 +60,7 @@ public class QuiltJoinedFileSystem extends QuiltBaseFileSystem<QuiltJoinedFileSy
 			current = 0;
 		}
 		uniqueNames.put(name, current);
-		return name + "." + current;
+		return name + current;
 	}
 
 	@Override
