d271da151c24a12a124feeff42b1c579f6e98b52
Haven <haven@hephaestus.dev>
2022-05-10 10:58:01 -0700

    Fix system property for default config extension and add some comments

--- src/main/java/org/quiltmc/loader/impl/util/SystemProperties.java
+++ src/main/java/org/quiltmc/loader/impl/util/SystemProperties.java
@@ -26,7 +26,9 @@ public final class SystemProperties {
 	public static final String DEBUG_MOD_SOLVING = "loader.debug.mod_solving";
 	public static final String MODS_DIRECTORY = "loader.modsDir";
 	public static final String CONFIG_DIRECTORY = "loader.configDir";
-	public static final String DEFAULT_CONFIG_EXTENSION = "loader.globalConfigExtension";
+	// the file extension to be used for configs that do not explicitly declare a specific extension
+	public static final String DEFAULT_CONFIG_EXTENSION = "loader.defaultConfigExtension";
+	// the file extension to use for ALL configs, overriding explicit defaults
 	public static final String GLOBAL_CONFIG_EXTENSION = "loader.globalConfigExtension";
 	public static final String LOG_FILE = "loader.log.file";
 	public static final String LOG_LEVEL = "loader.log.level";
