4363159bbd8fa76a3f4e347264c3810469e99156
Una <me@unascribed.com>
2022-11-05 21:42:13 -0700

    Implement version-aware sorting for GenericVersionImpl

--- src/main/java/org/quiltmc/loader/impl/metadata/qmj/GenericVersionImpl.java
+++ src/main/java/org/quiltmc/loader/impl/metadata/qmj/GenericVersionImpl.java
@@ -53,10 +53,86 @@ public class GenericVersionImpl implements Version.Raw {
 	public int compareTo(Version other) {
 		return compareRaw(raw(), other.raw());
 	}
-
+	// it's difficult to generically deal with comparables - these operations are safe
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static int compareRaw(String a, String b) {
-		// Ideally this would use number-aware comparison
-		// Oh well
-		return a.compareTo(b);
+		// Arrays.compare gives wrong precedence to array length, causing 1.0.1 to sort as older than 1.0.0_01
+		// so, implement it ourselves from scratch
+		List<Comparable<?>> ad = decompose(a);
+		List<Comparable<?>> bd = decompose(b);
+		for (int i = 0; i < Math.max(ad.size(), bd.size()); i++) {
+			Comparable ac = i >= ad.size() ? null : ad.get(i);
+			Comparable bc = i >= bd.size() ? null : bd.get(i);
+			if (typeof(ac) != typeof(bc)) {
+				// Comparables assume the input object is the same type as the object, so we need to
+				// ensure that's true; this will happen in the case of two critically mismatched
+				// versions that contain a symbol where a number is expected - in this case, lexical
+				// is the best we can do
+				ac = ac == null ? null : ac.toString();
+				bc = bc == null ? null : bc.toString();
+			}
+			int c;
+			if (bc == null && isSemverPrerelease(ac)) {
+				// special case: compatibility with semver, which sorts "pre-releases" differently
+				c = -1;
+			} else if (ac == null && isSemverPrerelease(bc)) {
+				c = 1;
+			} else if (ac == null) {
+				// special case: nulls are *always* lesser
+				// bc cannot be null here, don't need to check
+				c = -1;
+			} else {
+				c = bc == null ? 1 : ac.compareTo(bc);
+			}
+			if (c != 0) return c;
+		}
+		return 0;
+	}
+	
+	/*
+	 * Break apart a string into "logical" version components, by splitting it where a string
+	 * of characters changes from numeric to non-numeric.
+	 */
+	private static List<Comparable<?>> decompose(String str) {
+		if (str.isEmpty()) return List.of();
+		boolean lastWasNumber = Character.isDigit(str.codePointAt(0));
+		StringBuilder accum = new StringBuilder();
+		List<Comparable<?>> out = new ArrayList<>();
+		// remove appendices
+		int plus = str.lastIndexOf('+');
+		if (plus != -1) str = str.substring(0, plus);
+		for (int cp : str.codePoints().toArray()) {
+			boolean number = Character.isDigit(cp);
+			if (number != lastWasNumber) {
+				complete(lastWasNumber, accum, out);
+				lastWasNumber = number;
+			}
+			accum.appendCodePoint(cp);
+		}
+		complete(lastWasNumber, accum, out);
+		return out;
+	}
+
+	private static void complete(boolean number, StringBuilder accum, List<Comparable<?>> out) {
+		String s = accum.toString();
+		if (number) {
+			// just in case someone uses a pointlessly long version string...
+			out.add(Long.parseLong(s));
+		} else {
+			out.add(s);
+		}
+		accum.setLength(0);
+	}
+	
+	private static boolean isSemverPrerelease(Object o) {
+		if (o instanceof String) {
+			String s = (String)o;
+			return s.length() > 1 && s.charAt(0) == '-';
+		}
+		return false;
+	}
+
+	private static Class<?> typeof(Object o) {
+		return o == null ? null : o.getClass();
 	}
 }
