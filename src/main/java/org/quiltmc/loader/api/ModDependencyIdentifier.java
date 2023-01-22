83b347a64693885c4f36b08ff589ab2f13fbbff3
Glitch <glitch.g3431@gmail.com>
2023-01-21 20:08:13 -0600

    Update licenses

--- src/main/java/org/quiltmc/loader/api/ModDependencyIdentifier.java
+++ src/main/java/org/quiltmc/loader/api/ModDependencyIdentifier.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016 FabricMC
- * Copyright 2022 QuiltMC
+ * Copyright 2022-2023 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
