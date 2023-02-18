44e985ed7cb33103836f7aa7b060031e0086354b
AlexIIL <alexj9@me.com>
2023-02-18 23:23:40 +0000

    Version bump to 79.

--- src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
+++ src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
@@ -119,7 +119,7 @@ public final class QuiltLoaderImpl {
 
 	public static final int ASM_VERSION = Opcodes.ASM9;
 
-	public static final String VERSION = "0.18.1-beta.78";
+	public static final String VERSION = "0.18.1-beta.79";
 	public static final String MOD_ID = "quilt_loader";
 	public static final String DEFAULT_MODS_DIR = "mods";
 	public static final String DEFAULT_CONFIG_DIR = "config";
