39b77a4b150062c21c24a04aa2337c33f8ba166d
AlexIIL <alexj9@me.com>
2023-04-05 06:31:09 +0100

    Undelete

---
+++ src/main/java/org/quiltmc/loader/impl/metadata/qmj/QuiltModMetadataWrapperFabric.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2022, 2023 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.quiltmc.loader.impl.metadata.qmj;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.plugin.ModContainerExt;
+import org.quiltmc.loader.impl.metadata.FabricLoaderModMetadata;
+import org.quiltmc.loader.impl.metadata.GeneralExt2FabricMetadata;
+import org.quiltmc.loader.impl.util.QuiltLoaderInternal;
+import org.quiltmc.loader.impl.util.QuiltLoaderInternalType;
+
+@QuiltLoaderInternal(QuiltLoaderInternalType.LEGACY_EXPOSED)
+public class QuiltModMetadataWrapperFabric extends GeneralExt2FabricMetadata implements FabricLoaderModMetadata {
+
+	public QuiltModMetadataWrapperFabric(InternalModMetadata quiltMeta, ModContainer quiltContainer) {
+		super(quiltMeta, (ModContainerExt) quiltContainer);
+	}
+}
