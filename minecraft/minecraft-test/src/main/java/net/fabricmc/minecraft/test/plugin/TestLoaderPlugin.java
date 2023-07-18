cb6f2204e989bd59824ad8caad129a985d7a3ff0
AlexIIL <alexj9@me.com>
2023-07-18 08:30:23 +0100

    Add (disabled) loader plugin to the test mod.

---
+++ minecraft/minecraft-test/src/main/java/net/fabricmc/minecraft/test/plugin/TestLoaderPlugin.java
@@ -0,0 +1,21 @@
+package net.fabricmc.minecraft.test.plugin;
+
+import java.util.Map;
+
+import org.quiltmc.loader.api.LoaderValue;
+import org.quiltmc.loader.api.plugin.QuiltLoaderPlugin;
+import org.quiltmc.loader.api.plugin.QuiltPluginContext;
+
+public class TestLoaderPlugin implements QuiltLoaderPlugin {
+
+    @Override
+    public void load(QuiltPluginContext arg0, Map<String, LoaderValue> arg1) {
+
+    }
+
+    @Override
+    public void unload(Map<String, LoaderValue> arg0) {
+
+    }
+
+}
