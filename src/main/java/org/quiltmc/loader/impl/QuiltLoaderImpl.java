c7104d5dfe8b54385665523fe1362e2106ec7c31
AlexIIL <alexj9@me.com>
2023-11-27 14:31:11 +0000

    Bump version for release

--- src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
+++ src/main/java/org/quiltmc/loader/impl/QuiltLoaderImpl.java
@@ -130,7 +130,7 @@ public final class QuiltLoaderImpl {
 
 	public static final int ASM_VERSION = Opcodes.ASM9;
 
-	public static final String VERSION = "0.22.0-beta.3";
+	public static final String VERSION = "0.22.0";
 	public static final String MOD_ID = "quilt_loader";
 	public static final String DEFAULT_MODS_DIR = "mods";
 	public static final String DEFAULT_CACHE_DIR = ".cache";
