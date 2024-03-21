87d200b681ceb09193dbc95a2e57d8bc22e3361f
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2024-03-21 18:31:11 +0100

    Blah Blah Blah Usage Conventions

--- src/main/java/org/quiltmc/loader/api/minecraft/Requires.java
+++ src/main/java/org/quiltmc/loader/api/minecraft/Requires.java
@@ -18,7 +18,15 @@ import java.lang.annotation.Target;
  * from the overridden class only when specified mods are found.
  * <p>
  * When applied to an implemented interface (via {@link ElementType#TYPE_USE}) it will be removed when running without the specified mods.
- * Overridden interface methods need to be annotated separately in order to be removed. */
+ * Overridden interface methods need to be annotated separately in order to be removed.
+ *
+ * @apiNote Even is this annotation is provided by Quilt Loader, some usages of it are not recommended. In example, when
+ * you are using code from other mods thanks to their APIs using entrypoints, you should not implement their entrypoint
+ * interface on the same classes that you are using for vanilla entrypoints. Instead, you should use a separate class in
+ * a separated package, dedicated to the mod integration that you are making. And to continue this example by a great usage
+ * of this annotation, you could use it in a new package-info.java file of the current package dedicated for the
+ * mod integration, in order to trim the complete package, which will provide an additional safety layer by avoiding
+ * errors when loading and / or calling classes and elements that should not when the required mod is not present. */
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.TYPE, ElementType.TYPE_USE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.PACKAGE })
 @Documented
