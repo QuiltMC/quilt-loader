ee9942b830423ec91924e86826827e24260637eb
Eli Orona <eliorona@live.com>
2024-08-09 23:09:45 -0700

    Finished QMJ Writer

--- src/main/java/org/quiltmc/loader/impl/metadata/qmj/InternalModMetadataJsonWriter.java
+++ src/main/java/org/quiltmc/loader/impl/metadata/qmj/InternalModMetadataJsonWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022, 2023 QuiltMC
+ * Copyright 2024 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,26 +18,41 @@ package org.quiltmc.loader.impl.metadata.qmj;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.jetbrains.annotations.NotNull;
 import org.quiltmc.json5.JsonWriter;
 import org.quiltmc.loader.api.LoaderValue;
+import org.quiltmc.loader.api.ModContributor;
 import org.quiltmc.loader.api.ModDependency;
+import org.quiltmc.loader.api.ModLicense;
+import org.quiltmc.loader.api.ModMetadata;
 import org.quiltmc.loader.api.VersionConstraint;
 import org.quiltmc.loader.api.VersionRange;
+import org.quiltmc.loader.api.plugin.ModMetadataExt;
+import org.quiltmc.loader.impl.metadata.FabricLoaderModMetadata;
 import org.quiltmc.loader.impl.util.QuiltLoaderInternal;
 import org.quiltmc.loader.impl.util.QuiltLoaderInternalType;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.metadata.ModEnvironment;
+
 @QuiltLoaderInternal(QuiltLoaderInternalType.NEW_INTERNAL)
 public class InternalModMetadataJsonWriter {
+	private static final List<String> SPECIFIC_KEY_ORDER = Stream.of("schema_version", "quilt_loader", "mixin", "access_widener", "minecraft").collect(Collectors.toList());
 
 	public static void write(InternalModMetadata mod, Writer writer) throws IOException {
 		JsonWriter json = JsonWriter.json(writer);
@@ -50,140 +65,125 @@ public class InternalModMetadataJsonWriter {
 	}
 
 	public static LoaderValue toJson(InternalModMetadata mod) {
-		// Put all the custom values (+more if QMJ impl). If this is empty then we cannot get the custom values anyway.
 		Map<String, LoaderValue> values = new TreeMap<>((o1, o2) -> {
-			// Force schema then quilt_loader
-			if (o1.equals("schemaVersion")) {
-				return -1;
-			} else if (o2.equals("schemaVersion")) {
-				return 1;
+			if (o1.equals(o2)) {
+				return 0;
 			}
 
-			if (o1.equals("quilt_loader")) {
-				return -1;
-			} else if (o2.equals("quilt_loader")) {
-				return 1;
+			for (String key : SPECIFIC_KEY_ORDER) {
+				if (o1.equals(key)) {
+					return -1;
+				} else if (o2.equals(key)) {
+					return 1;
+				}
 			}
 
 			return o1.compareTo(o2);
 		});
-		values.putAll(mod.values());
 
-		if (!values.containsKey("schemaVersion")) {
-			values.put("schemaVersion", new JsonLoaderValue.NumberImpl("", 1));
+		values.put("schema_version", new JsonLoaderValue.NumberImpl("", 1));
+		values.put("quilt_loader", createQuiltLoader(mod));
+
+		LoaderValue mixin = createMixins(mod);
+		if (mixin != null) {
+			values.put("mixin", mixin);
 		}
 
-		// Implementation does not have quilt_loader tag (likely FMJ wrapper)
-		if (!values.containsKey("quilt_loader")) {
-			Map<String, LoaderValue> quilt_loader = new LinkedHashMap<>();
-			quilt_loader.put("id", string(mod.id()));
-			if (!mod.group().equals("loader.fabric")) {
-				quilt_loader.put("group", string(mod.group()));
+		LoaderValue accessWidener = createAccessWidener(mod);
+		if (accessWidener != null) {
+			values.put("access_widener", accessWidener);
+		}
+
+		LoaderValue minecraft = createMinecraft(mod);
+		if (minecraft != null) {
+			values.put("minecraft", minecraft);
+		}
+
+		// Put all the custom values (+more if QMJ impl). If this is empty then we cannot get the custom values anyway.
+		mod.values().forEach((s, loaderValue) -> {
+			if (!values.containsKey(s)) {
+				values.put(s, loaderValue);
 			}
-			quilt_loader.put("version", string(mod.version().raw()));
-			quilt_loader.put("entrypoints",
-					object(
-							mod
-									.getEntrypoints()
-									.entrySet()
-									.stream()
-									.collect(
-											Collectors.toMap(
-													Map.Entry::getKey,
-													entry -> array(entry.getValue().stream().map(entrypoint -> {
-														if (entrypoint instanceof AdapterLoadableClassEntry) {
-															AdapterLoadableClassEntry adapted = (AdapterLoadableClassEntry) entrypoint;
-															if (adapted.getAdapter().equals("default")) {
-																return string(adapted.getValue());
-															} else {
-																Map<String, LoaderValue> value = new LinkedHashMap<>();
-																value.put("adapter", string(adapted.getAdapter()));
-																value.put("value", string(adapted.getValue()));
-																return object(value);
-															}
-														}
-														return new JsonLoaderValue.NullImpl("");
-													})))
-									)
-					)
-			);
-			quilt_loader.put("jars", array(mod.jars().stream().map(InternalModMetadataJsonWriter::string)));
-			quilt_loader.put("language_adapters", object(mod.languageAdapters().entrySet().stream().collect(Collectors.toMap(
-					Map.Entry::getKey,
-					adapter -> string(adapter.getValue())
-			))));
-			quilt_loader.put("depends", array(mod.depends().stream().map(InternalModMetadataJsonWriter::modDependency)));
-			quilt_loader.put("breaks", array(mod.breaks().stream().map(InternalModMetadataJsonWriter::modDependency)));
-			quilt_loader.put("repositories", array(mod.repositories().stream().map(InternalModMetadataJsonWriter::string)));
-			quilt_loader.put("load_type", string(mod.loadType().name().toLowerCase()));
-			quilt_loader.put("provides", array(mod.provides().stream().map(provided -> {
-				if (provided.version().equals(mod.version()) && !provided.id().contains(":")) {
-					return string(provided.id());
-				}
-
-				Map<String, LoaderValue> value = new LinkedHashMap<>();
-				value.put("id", string(provided.id().substring(provided.id().indexOf(":" + 1))));
-				if (provided.id().contains(":")) {
-					value.put("group", string(provided.id().substring(0, provided.id().indexOf(":"))));
-				}
-				value.put("version", string(provided.version().raw()));
-
-				return object(value);
-			})));
-			quilt_loader.put("intermediate_mappings", string(mod.intermediateMappings()));
-			// TODO: metadata
-
-			values.put("quilt_loader", object(quilt_loader));
-		}
-
-		// TODO: Add Mixins
-		// TODO: Add AW
-
-		// TODO: Add Environment?
+		});
 
 		return object(values);
 	}
 
-	@NotNull
-	private static JsonLoaderValue array(Stream<LoaderValue> values) {
-		return new JsonLoaderValue.ArrayImpl("", values.collect(Collectors.toList()));
+	private static LoaderValue createQuiltLoader(InternalModMetadata mod) {
+		return object(quiltLoader -> {
+
+			quiltLoader.put("id", string(mod.id()));
+			// This is required but there isn't a good way to set it without knowing
+			quiltLoader.put("group", string(mod.group()));
+			quiltLoader.put("version", string(mod.version().raw()));
+
+			quiltLoader.put("metadata", createMetadata(mod));
+
+			putEmptyMap("entrypoints", mod.getEntrypoints(), k -> k, InternalModMetadataJsonWriter::entrypoints, quiltLoader);
+
+			putEmptyCollection("depends", mod.depends(), InternalModMetadataJsonWriter::modDependency, quiltLoader);
+			putEmptyCollection("breaks", mod.breaks(), InternalModMetadataJsonWriter::modDependency, quiltLoader);
+
+			putEmptyCollection("provides", mod.provides(), InternalModMetadataJsonWriter.provided(mod), quiltLoader);
+
+			if (mod.loadType() != ModMetadataExt.ModLoadType.IF_REQUIRED) {
+				quiltLoader.put("load_type", string(mod.loadType().name().toLowerCase()));
+			}
+
+			if (!mod.intermediateMappings().equals("org.quiltmc:hashed")) {
+				quiltLoader.put("intermediate_mappings", string(mod.intermediateMappings()));
+			}
+
+			putEmptyCollection("jars", mod.jars(), InternalModMetadataJsonWriter::string, quiltLoader);
+			putEmptyCollection("repositories", mod.repositories(), InternalModMetadataJsonWriter::string, quiltLoader);
+
+			putEmptyMap("language_adapters", mod.languageAdapters(), k -> k, InternalModMetadataJsonWriter::string, quiltLoader);
+		});
 	}
 
-	@NotNull
-	private static JsonLoaderValue array(List<LoaderValue> values) {
-		return new JsonLoaderValue.ArrayImpl("", values);
-	}
+	private static JsonLoaderValue entrypoints(Collection<ModMetadataExt.ModEntrypoint> entrypoints) {
+		return array(entrypoints.stream().map(entrypoint -> {
+			if (entrypoint instanceof AdapterLoadableClassEntry) {
+				AdapterLoadableClassEntry adapted = (AdapterLoadableClassEntry) entrypoint;
+				if (adapted.getAdapter().equals("default")) {
+					return string(adapted.getValue());
+				}
 
-	@NotNull
-	private static JsonLoaderValue object(Map<String, LoaderValue> quilt_loader) {
-		return new JsonLoaderValue.ObjectImpl("", quilt_loader);
+				return object(value -> {
+					value.put("adapter", string(adapted.getAdapter()));
+					value.put("value", string(adapted.getValue()));
+				});
+			}
+
+			throw new IllegalStateException("Unknown Mod Dependency type! " + entrypoint.getClass().getName());
+		}));
 	}
 
 	private static JsonLoaderValue modDependency(ModDependency dep) {
 		if (dep instanceof ModDependency.Only) {
 			ModDependency.Only only = (ModDependency.Only) dep;
-			Map<String, LoaderValue> value = new LinkedHashMap<>();
-			value.put("id", string(only.id().toString()));
+			JsonLoaderValue.ObjectImpl obj = object(value -> {
+				value.put("id", string(only.id().toString()));
 
+				if (only.optional()) {
+					value.put("optional", new JsonLoaderValue.BooleanImpl("", only.optional()));
+				}
+				if (!only.versionRange().equals(VersionRange.ANY)) {
+					value.put("version", version(only.versionRange()));
+				}
+				if (!only.reason().isEmpty()) {
+					value.put("reason", string(only.reason()));
+				}
+				if (only.unless() != null) {
+					value.put("unless", modDependency(only.unless()));
+				}
+			});
 
-			if (only.optional()) {
-				value.put("optional", new JsonLoaderValue.BooleanImpl("", only.optional()));
-			}
-			if (!only.versionRange().equals(VersionRange.ANY)) {
-				value.put("version", version(only.versionRange()));
-			}
-			if (!only.reason().isEmpty()) {
-				value.put("reason", string(only.reason()));
-			}
-			if (only.unless() != null) {
-				value.put("unless", modDependency(only.unless()));
+			if (obj.size() == 1) {
+				return obj.get("id");
 			}
 
-			if (value.size() == 1) {
-				return (JsonLoaderValue) value.get("id");
-			}
-
-			return object(value);
+			return obj;
 		} else if (dep instanceof ModDependency.Any || dep instanceof ModDependency.All) {
 			Collection<ModDependency.Only> deps = (Collection<ModDependency.Only>) dep;
 			return array(deps.stream().map(InternalModMetadataJsonWriter::modDependency));
@@ -192,17 +192,244 @@ public class InternalModMetadataJsonWriter {
 		throw new IllegalStateException("Unknown Mod Dependency type! " + dep.getClass().getName());
 	}
 
-	@NotNull
-	private static JsonLoaderValue string(String only) {
-		return new JsonLoaderValue.StringImpl("", only);
-	}
-
-	// TODO: test this
 	private static JsonLoaderValue version(VersionRange range) {
+		// TODO: test this
 		if (range.equals(VersionRange.ANY)) {
 			return string("*");
 		}
 
 		return array(range.convertToConstraints().stream().map(VersionConstraint::toString).map(InternalModMetadataJsonWriter::string));
+	}
+
+	private static <P extends ModMetadata.ProvidedMod> Function<P, LoaderValue> provided(InternalModMetadata mod) {
+		return provided -> {
+			if (provided.version().equals(mod.version()) && !provided.id().contains(":")) {
+				return string(provided.id());
+			}
+
+			Map<String, LoaderValue> value = new LinkedHashMap<>();
+			value.put("id", string(provided.id().substring(provided.id().indexOf(":" + 1))));
+			if (provided.id().contains(":")) {
+				value.put("group", string(provided.id().substring(0, provided.id().indexOf(":"))));
+			}
+
+			value.put("version", string(provided.version().raw()));
+
+			return object(value);
+		};
+	}
+
+	private static JsonLoaderValue createMetadata(InternalModMetadata mod) {
+		return object(metadata -> {
+			metadata.put("name", string(mod.name()));
+			if (!mod.description().isEmpty()) {
+				metadata.put("description", string(mod.description()));
+			}
+
+			putEmptyMap("contributors", mod.contributors(), ModContributor::name, contributor -> {
+				if (contributor.roles().size() == 1) {
+					return string(contributor.roles().iterator().next());
+				}
+				return array(contributor.roles().stream().map(InternalModMetadataJsonWriter::string));
+			}, metadata);
+
+			putEmptyMap("contact", mod.contactInfo(), Function.identity(), InternalModMetadataJsonWriter::string, metadata);
+
+			// Special case a single license
+			if (mod.licenses().size() == 1) {
+				ModLicense license = mod.licenses().iterator().next();
+				metadata.put("licence", createLicense(license));
+			} else {
+				putEmptyCollection("license", mod.licenses(), InternalModMetadataJsonWriter::createLicense, metadata);
+			}
+
+
+			LoaderValue iconJson = getIcon(mod);
+			if (iconJson != null) {
+				metadata.put("icon", iconJson);
+			}
+		});
+	}
+
+	private static JsonLoaderValue createLicense(ModLicense license) {
+		// Object equality here is fine because it comes from a map
+		if (ModLicense.fromIdentifier(license.id()) == license) {
+			return string(license.id());
+		}
+
+		return object(licenseObj -> {
+			licenseObj.put("name", string(license.name()));
+			licenseObj.put("id", string(license.id()));
+			licenseObj.put("url", string(license.url()));
+			licenseObj.put("description", string(license.description()));
+		});
+	}
+
+	private static LoaderValue getIcon(InternalModMetadata mod) {
+		// Cursed time!
+		if (mod instanceof V1ModMetadataImpl) {
+			try {
+				Field iconsField = mod.getClass().getDeclaredField("icons");
+				iconsField.setAccessible(true);
+				Icons icons = (Icons) iconsField.get(mod);
+				if (icons instanceof Icons.Single) {
+					String icon = icons.getIcon(0);
+					if (icon == null) {
+						return null;
+					}
+
+					return string(icon);
+				} else if (icons instanceof Icons.Multiple) {
+					Field mapField = Icons.Multiple.class.getDeclaredField("icons");
+					mapField.setAccessible(true);
+					SortedMap<Integer, String> map = (SortedMap<Integer, String>) mapField.get(icons);
+					return object(obj -> map.forEach((size, icon) -> obj.put(Integer.toString(size), string(icon))));
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		} else if (mod instanceof FabricModMetadataWrapper) {
+			FabricLoaderModMetadata fabric = mod.asFabricModMetadata();
+			if (fabric.getClass().getSimpleName().equals("V1ModMetadataFabric")) {
+				try {
+					// V1ModMetadataFabric.IconEntry
+					Field iconField = fabric.getClass().getDeclaredField("icon");
+					iconField.setAccessible(true);
+					Object icons = iconField.get(fabric);
+
+					if (icons.getClass().getName().contains("Single")) {
+						Field stringField = icons.getClass().getDeclaredField("icon");
+						stringField.setAccessible(true);
+						return string((String) stringField.get(icons));
+					} else if (icons.getClass().getName().contains("MapEntry")) {
+						Field iconsField = icons.getClass().getDeclaredField("icons");
+						iconsField.setAccessible(true);
+						SortedMap<Integer, String> map = (SortedMap<Integer, String>) iconsField.get(icons);
+						return object(obj -> map.forEach((size, icon) -> obj.put(Integer.toString(size), string(icon))));
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private static LoaderValue createMixins(InternalModMetadata mod) {
+		Set<String> client = new HashSet<>(mod.mixins(EnvType.CLIENT));
+		Set<String> server = new HashSet<>(mod.mixins(EnvType.SERVER));
+
+		if (client.isEmpty() && server.isEmpty()) {
+			return null;
+		}
+
+		Set<String> shared = client.stream().filter(server::contains).collect(Collectors.toSet());
+
+		LoaderValue mixin;
+		if (client.size() == 1 && server.size() == 1 && shared.size() == 1) { // Client and server both have the same entry
+			mixin = string(shared.iterator().next());
+		} else if (client.size() == 1 && server.isEmpty()) { // Client only mixin
+			mixin = mixinObject(client.iterator().next(), "client");
+		} else if (server.size() == 1 && client.isEmpty()) { // Server only mixin
+			mixin = mixinObject(server.iterator().next(), "dedicated_server");
+		} else { // We know we have an array
+			List<LoaderValue> mixins = new ArrayList<>();
+			for (String config : shared) {
+				mixins.add(string(config));
+			}
+
+			for (String config : client) {
+				if (!shared.contains(config)) {
+					mixins.add(mixinObject(config, "client"));
+				}
+			}
+
+			for (String config : server) {
+				if (!shared.contains(config)) {
+					mixins.add(mixinObject(config, "dedicated_server"));
+				}
+			}
+
+			mixin = array(mixins);
+		}
+
+		return mixin;
+	}
+
+	private static JsonLoaderValue mixinObject(String config, String env) {
+		return object(objectValues -> {
+			objectValues.put("config", string(config));
+			objectValues.put("environment", string(env));
+		});
+	}
+
+	private static LoaderValue createAccessWidener(InternalModMetadata mod) {
+		if (mod.accessWideners().isEmpty()) {
+			return null;
+		}
+
+		if (mod.accessWideners().size() == 1) {
+			return string(mod.accessWideners().iterator().next());
+		} else {
+			return array(mod.accessWideners().stream().map(InternalModMetadataJsonWriter::string));
+		}
+	}
+
+	private static LoaderValue createMinecraft(InternalModMetadata mod) {
+		if (mod.environment().equals(ModEnvironment.UNIVERSAL)) {
+			return null;
+		}
+
+		return object(minecraft -> {
+			switch (mod.environment()) {
+				case CLIENT:
+					minecraft.put("environment", string("client"));
+					break;
+				case SERVER:
+					minecraft.put("environment", string("dedicated_server"));
+					break;
+			}
+		});
+	}
+
+	private static <T> void putEmptyCollection(String field, Collection<T> collection, Function<T, ? extends LoaderValue> converter, Map<String, LoaderValue> object) {
+		if (!collection.isEmpty()) {
+			object.put(field, array(collection.stream().map(converter)));
+		}
+	}
+
+	private static <K, V> void putEmptyMap(String field, Map<K, V> map, Function<K, String> key, Function<V, ? extends LoaderValue> value, Map<String, LoaderValue> object) {
+		if (!map.isEmpty()) {
+			object.put(field, object(obj -> map.forEach((k, v) -> obj.put(key.apply(k), value.apply(v)))));
+		}
+	}
+
+	private static <T> void putEmptyMap(String field, Collection<T> map, Function<T, String> key, Function<T, ? extends LoaderValue> value, Map<String, LoaderValue> object) {
+		if (!map.isEmpty()) {
+			object.put(field, object(obj -> map.forEach((t) -> obj.put(key.apply(t), value.apply(t)))));
+		}
+	}
+
+	private static JsonLoaderValue.StringImpl string(String only) {
+		return new JsonLoaderValue.StringImpl("", only);
+	}
+
+	private static JsonLoaderValue.ArrayImpl array(List<LoaderValue> values) {
+		return new JsonLoaderValue.ArrayImpl("", values);
+	}
+
+	private static JsonLoaderValue.ArrayImpl array(Stream<LoaderValue> values) {
+		return array(values.collect(Collectors.toList()));
+	}
+
+	private static JsonLoaderValue.ObjectImpl object(Map<String, LoaderValue> values) {
+		return new JsonLoaderValue.ObjectImpl("", values);
+	}
+
+	private static JsonLoaderValue.ObjectImpl object(Consumer<Map<String, LoaderValue>> consumer) {
+		Map<String, LoaderValue> values = new LinkedHashMap<>();
+		consumer.accept(values);
+		return object(values);
 	}
 }
--- src/test/java/org/quiltmc/loader/impl/metadata/qmj/InternalModMetadataJsonWriterTest.java
+++ src/test/java/org/quiltmc/loader/impl/metadata/qmj/InternalModMetadataJsonWriterTest.java
@@ -1,33 +1,62 @@
 package org.quiltmc.loader.impl.metadata.qmj;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.quiltmc.loader.impl.fabric.metadata.FabricModMetadataReader;
 import org.quiltmc.loader.impl.fabric.metadata.ParseMetadataException;
-import org.quiltmc.loader.impl.metadata.FabricLoaderModMetadata;
 
 class InternalModMetadataJsonWriterTest {
 
-	@Test
-	void toJson() throws IOException, ParseMetadataException {
-		FabricLoaderModMetadata read = FabricModMetadataReader.parseMetadata(new File(System.getProperty("user.dir")).toPath()
-				.resolve("src")
-				.resolve("test")
-				.resolve("resources")
-				.resolve("testing")
-				.resolve("parsing")
-				.resolve("fabric")
-				.resolve("spec")
-				.resolve("long.json"));
+	static Stream<Path> fabricJsons() throws IOException {
+		Path specJsons = new File(System.getProperty("user.dir")).toPath()
+				.resolve("src/test/resources/testing/parsing/fabric/spec");
 
+		return StreamSupport.stream(Files.newDirectoryStream(specJsons).spliterator(), false);
+	}
+
+	@ParameterizedTest()
+	@MethodSource("org.quiltmc.loader.impl.metadata.qmj.InternalModMetadataJsonWriterTest#fabricJsons")
+	void fabricToJson(Path path) throws IOException, ParseMetadataException {
+		InternalModMetadata read = (InternalModMetadata) FabricModMetadataReader.parseMetadata(path).asQuiltModMetadata();
 
 		StringWriter writer = new StringWriter();
-		InternalModMetadataJsonWriter.write(((InternalModMetadata) read.asQuiltModMetadata()), writer);
-		System.out.println(writer);
+		InternalModMetadataJsonWriter.write(read, writer);
+		String output = writer.toString();
+		System.out.println(output);
+
+		JsonLoaderValue.ObjectImpl json = (JsonLoaderValue.ObjectImpl) JsonLoaderFactoryImpl.INSTANCE.read(new ByteArrayInputStream(output.getBytes()));
+		V1ModMetadataImpl readQuilt = V1ModMetadataReader.read(json);
+	}
+
+	static Stream<Path> quiltJsons() throws IOException {
+		Path specJsons = new File(System.getProperty("user.dir")).toPath()
+				.resolve("src/test/resources/testing/parsing/quilt/v1/auto/spec");
+
+		return StreamSupport.stream(Files.newDirectoryStream(specJsons).spliterator(), false);
+	}
+
+	@ParameterizedTest()
+	@MethodSource("org.quiltmc.loader.impl.metadata.qmj.InternalModMetadataJsonWriterTest#quiltJsons")
+	void quiltToJson(Path path) throws IOException {
+		JsonLoaderValue.ObjectImpl json = (JsonLoaderValue.ObjectImpl) JsonLoaderFactoryImpl.INSTANCE.read(Files.newInputStream(path));
+		InternalModMetadata read = V1ModMetadataReader.read(json).asQuiltModMetadata();
+
+		StringWriter writer = new StringWriter();
+		InternalModMetadataJsonWriter.write(read, writer);
+		String output = writer.toString();
+		System.out.println(output);
+
+		json = (JsonLoaderValue.ObjectImpl) JsonLoaderFactoryImpl.INSTANCE.read(new ByteArrayInputStream(output.getBytes()));
+		V1ModMetadataImpl readQuilt = V1ModMetadataReader.read(json);
 	}
 }
