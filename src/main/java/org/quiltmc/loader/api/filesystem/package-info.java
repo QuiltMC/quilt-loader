d67ec62cef70fb047d0b990938abbde387e8b721
AlexIIL <alexj9@me.com>
2025-09-03 01:53:45 +0100

    Add missing license headers.

--- src/main/java/org/quiltmc/loader/api/filesystem/package-info.java
+++ src/main/java/org/quiltmc/loader/api/filesystem/package-info.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2025 QuiltMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /** New subpackage for quilt's filesystem APIs.
  * <p>
  * There are 7 older api files which are not part of this package for backwards compatibility:
  * <ul>
  * <li>{@link org.quiltmc.loader.api.CachedFileSystem}</li>
  * <li>{@link org.quiltmc.loader.api.ExtendedFiles}</li>
-* <li>{@link org.quiltmc.loader.api.ExtendedFileSystem}</li>
+ * <li>{@link org.quiltmc.loader.api.ExtendedFileSystem}</li>
  * <li>{@link org.quiltmc.loader.api.FasterFiles}</li>
  * <li>{@link org.quiltmc.loader.api.FasterFileSystem}</li>
  * <li>{@link org.quiltmc.loader.api.MountOption}</li>
